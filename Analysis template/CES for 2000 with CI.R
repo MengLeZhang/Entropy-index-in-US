e8d8ff6ddb644a4731b0e3d4fc61fbcffbdf40a4
Meng Le <mzhang@hotmail.co.uk>
2019-04-09 14:26:45 +0100

    Changed some code for the MCMC

--- Analysis template/CES for 2000 with CI.R
+++ Analysis template/CES for 2000 with CI.R
@@ -5,6 +5,8 @@ library(doParallel)
 
 local.dir <- 'Local temp files'
 us2000.sf <- readRDS(local.dir %>% file.path('us2000_sf.RDS'))
+ci_function <- source('Analysis template/Template for analysis.R')
+
 
 msa.sf <-
   us2000.sf %>%
@@ -16,8 +18,8 @@ burnin <- 20000
 n.sample <- 40000 + burnin #because sample includes burin
 thin <- 10
 
+n.cores <- detectCores() - 1
 cl <- makePSOCKcluster(n.cores)
-
 
 foreach (i = 1:n.cores, .combine = c) %dopar% {
   library(tidyverse)
